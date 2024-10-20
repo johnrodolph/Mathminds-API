ce2ef8873b53e8344b446b0341bc544ba37747fd
Ribo <116358794+8maaan@users.noreply.github.com>
2024-10-20 09:17:57 +0800

    Added .gitignore

---
+++ Mathminds-API/src/test/java/com/IT334G4/Mathminds/MathmindsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IT334G4.Mathminds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathmindsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
